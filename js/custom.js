8d48599a1267d3ffd40cc41e7b6160e472b29f48
ombarus <ombarus.dev@gmail.com>
2018-10-31 22:00:37 +0900

    updated privacy policy for free version of diceDM and added a direct link to diceDM info page

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {  
-  var modals = ['#privacydicedm'];
+  var modals = ['#privacydicedm', '#portfolioModal-DiceDM'];
   if (window.location.hash && ~modals.indexOf(window.location.hash)) {
      $(window.location.hash).modal();
   }
