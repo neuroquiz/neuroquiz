a55094375e84fce749896de2b6dd97009da0b393
ombarus <ombarus.dev@gmail.com>
2018-11-01 20:50:35 +0900

    clean url because facebook has trouble with hashtag

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {  
   var modals = ['#privacydicedm', '#portfolioModal-DiceDM'];
+  if (window.location.pathname.includes("dicedmfree.html")) {
+	   $("#portfolioModal-DiceDM").modal();
+  }
   if (window.location.hash && ~modals.indexOf(window.location.hash)) {
      $(window.location.hash).modal();
   }
