aeb17db132cb1ff667073ef494b7e563dbbeee0b
MikeLippincott <1michaell2017@gmail.com>
2024-08-26 10:05:22 -0600

    dev

---
+++ create_jekyll_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create a new Jekyll environment
+# conda env create -f jekyll_env.yaml
+conda activate jekyll_env
+
+# Install Jekyll
+gem install jekyll bundler
+
+# test the installation by building a new Jekyll site
+bundle exec jekyll serve
